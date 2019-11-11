28479b9695542f924b384bd912d03367ba46befe
MJBackes <mjohnbackes@gmail.com>
2019-11-11 12:33:48 -0600

    Added classes for the TreeNode and the Tree itself. Also added the Add method and the Search method( along with an overload used in the main Search method).

---
+++ BinarySearchTree/BiSearchTree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BiSearchTree<T> where T:IComparable
+    {
+        public TreeNode<T> Root;
+
+        public BiSearchTree()
+        {
+
+        }
+        public void Add(TreeNode<T> input)
+        {
+            if(Root == null)
+            {
+                Root = input;
+                return;
+            }
+            TreeNode<T> temp = Root;
+            while (true)
+            {
+                if (input.value.CompareTo(temp.value) <= 0)
+                {
+                    if(temp.LeftChild == null)
+                    {
+                        temp.LeftChild = input;
+                        return;
+                    }
+                    else
+                    {
+                        temp = temp.LeftChild;
+                    }
+                }
+                else if (input.value.CompareTo(temp.value) > 0)
+                {
+                    if (temp.RightChild == null)
+                    {
+                        temp.RightChild = input;
+                        return;
+                    }
+                    else
+                    {
+                        temp = temp.RightChild;
+                    }
+                }
+            }
+        }
+        public bool Search(T input)
+        {
+            if (Root == null)
+                return false;
+            if (input.CompareTo(Root.value) == 0)
+            {
+                return true;
+            }
+            else if(input.CompareTo(Root.value) < 0)
+            {
+                return Search(input, Root.LeftChild);
+            }
+            else
+            {
+                return Search(input, Root.RightChild);
+            }
+
+        }
+        public bool Search(T input, TreeNode<T> subRoot)
+        {
+            if (subRoot == null)
+                return false;
+            if (input.CompareTo(subRoot.value) == 0)
+            {
+                return true;
+            }
+            else if (input.CompareTo(subRoot.value) < 0)
+            {
+                return Search(input, subRoot.LeftChild);
+            }
+            else
+            {
+                return Search(input, subRoot.RightChild);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BiSearchTree<int> tree = new BiSearchTree<int>();
+            tree.Add(new TreeNode<int>(5));
+            tree.Add(new TreeNode<int>(7));
+            tree.Add(new TreeNode<int>(9));
+            tree.Add(new TreeNode<int>(8));
+            tree.Add(new TreeNode<int>(3));
+            Console.WriteLine(tree.Search(8));
+            Console.WriteLine(tree.Search(5));
+            Console.WriteLine(tree.Search(6));
+            Console.WriteLine(tree.Search(7));
+            Console.WriteLine(tree.Search(9));
+            Console.WriteLine(tree.Search(3));
+            Console.ReadLine();
+        }
+    }
+}
