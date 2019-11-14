e53e4b30573bfe20b846e64ee63f7459609bfaa2
MJBackes <mjohnbackes@gmail.com>
2019-11-14 07:58:39 -0600

    Added an AddRecursive method that does what it sounds like it does. Also rewrote the original Add method to take in T objects instead of TreeNode<T>s becuase that seems like a better way to use it.

--- BinarySearchTree/BiSearchTree.cs
+++ BinarySearchTree/BiSearchTree.cs
@@ -10,22 +10,23 @@ namespace BinarySearchTree
     public class BiSearchTree<T> where T:IComparable
     {
         public TreeNode<T> Root;
-        public void Add(TreeNode<T> input)
+        public void Add(T input)
         {
             //O(n)
+            TreeNode<T> inputNode = new TreeNode<T>(input);
             if(Root == null)
             {
-                Root = input;
+                Root = inputNode;
                 return;
             }
             TreeNode<T> temp = Root;
             while (true)
             {
-                if (input.value.CompareTo(temp.value) <= 0)
+                if (input.CompareTo(temp.value) <= 0)
                 {
                     if(temp.LeftChild == null)
                     {
-                        temp.LeftChild = input;
+                        temp.LeftChild = inputNode;
                         return;
                     }
                     else
@@ -33,11 +34,11 @@ namespace BinarySearchTree
                         temp = temp.LeftChild;
                     }
                 }
-                else if (input.value.CompareTo(temp.value) > 0)
+                else if (input.CompareTo(temp.value) > 0)
                 {
                     if (temp.RightChild == null)
                     {
-                        temp.RightChild = input;
+                        temp.RightChild = inputNode;
                         return;
                     }
                     else
@@ -45,6 +46,35 @@ namespace BinarySearchTree
                         temp = temp.RightChild;
                     }
                 }
+            }
+        }
+        public void AddRecursive(T input)
+        {
+            //O(logn)
+            if (Root == null)
+                Root = new TreeNode<T>(input);
+            else if (input.CompareTo(Root.value) <= 0)
+            {
+              Root.LeftChild = AddRecursive(input, Root.LeftChild);
+            }
+            else
+            {
+               Root.RightChild = AddRecursive(input, Root.RightChild);
+            }
+        }
+        public TreeNode<T> AddRecursive(T input,TreeNode<T> subRoot)
+        {
+            if (subRoot == null)
+                return new TreeNode<T>(input);
+            else if (input.CompareTo(subRoot.value) <= 0)
+            {
+              subRoot.LeftChild = AddRecursive(input, subRoot.LeftChild);
+                return subRoot;
+            }
+            else
+            {
+               subRoot.RightChild = AddRecursive(input, subRoot.RightChild);
+                return subRoot;
             }
         }
         public bool Search(T input)
